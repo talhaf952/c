ec96e1aa2c963f66302bb0f4f353314d28f271ac
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-12 18:01:48 +0500

    task_2

---
+++ Lab_01_task2.c
@@ -0,0 +1,8 @@
+//Task 2: Write a program to print the following table using tab character:
+#include<stdio.h>
+int main()
+{
+    printf("First Name\tLast Name\tDegree\tRoll no\tBatch\nAli\t\t\tMunir\t\tDS\t\t001\t\tF22\n\nAyesha\t\tRazzaq\t\tDS\t\t002\t\tF22");
+    return 0;
+}
+
