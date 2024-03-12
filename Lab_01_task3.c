7239e3be5c96bd11589caeb06432a7df912ae3df
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-12 18:04:19 +0500

    task_3

---
+++ Lab_01_task3.c
@@ -0,0 +1,8 @@
+//Task 3: Write a program to generate the following output:
+#include<stdio.h>
+int main()
+{
+    printf("Practice 01\nProgramming Fundamentals.\nSecond Semester\nMy Programs\n\nFirst\t\tSecond\tThird.c");
+    return 0;
+}
+
