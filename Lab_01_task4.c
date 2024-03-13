09c23d986597348a2922b74d22a712bdcca45015
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-13 22:23:18 +0500

    Update Lab_01_task4.c

--- Lab_01_task4.c
+++ Lab_01_task4.c
@@ -1,7 +1,1 @@
-//Task 1: Write a program, which displays the following output:
-#include<stdio.h>
-int main()
-{
-    printf("Program Started…\nThis is the first line of my program.\nThis is the second line of my program.\nProgram Ended.");
-    return 0;
-}
+Task 4: Write a program to print the letter 'A'
