7acf24292d38995b381427b77115bb6df5ad808a
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-13 22:22:50 +0500

    task_4

---
+++ Lab_01_task4.c
@@ -0,0 +1,7 @@
+//Task 1: Write a program, which displays the following output:
+#include<stdio.h>
+int main()
+{
+    printf("Program Started…\nThis is the first line of my program.\nThis is the second line of my program.\nProgram Ended.");
+    return 0;
+}
