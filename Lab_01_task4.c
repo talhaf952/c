363488e4c441177a19869e8d91471a27cb147b05
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-16 16:48:14 +0500

    Update Lab_01_task4.c

--- Lab_01_task4.c
+++ Lab_01_task4.c
@@ -1,11 +1,10 @@
-Task 4: Write a program to print the letter 'A' using stars.
-
-  #include<stdio.h>
-  int main()
-  {
-
-  
-return 0;
+#include <stdio.h>
+int main() {
+    printf("Program to print A by Stars.\n");
+    printf("    *\n");
+    printf("   * *\n");
+    printf("  *****\n");
+    printf(" *      *\n");
+    printf("*        *\n");
+    return 0;
 }
-  
-
