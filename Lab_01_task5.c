ce75dae287768c9d024bff5eb4243df8ad0d70f0
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-16 16:49:06 +0500

    Task_05

---
+++ Lab_01_task5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("Program to print A by Stars.\n");
+    printf("* * * * *\n");
+    printf("* A B C *\n");
+    printf("* D E F *\n");
+    printf("* G H I *\n");
+    printf("* * * * *\n");
+    return 0;
+}
