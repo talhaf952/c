d8b58693f3afc6933a309444faa29a03984d8876
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-18 21:55:52 +0500

    task_2

---
+++ Lab_02_task2.c
@@ -0,0 +1,7 @@
+/*Task 2: Write a program, to calculate perimeter of an irregular connected shape having five sides. Hardcode the value of all sides in variables. Obtain perimeter by adding sides. Finally, print all the sides and perimeter*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello");
+}
