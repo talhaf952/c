74fa6dc403d47385177940132904a6306aeb1fd2
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-19 23:36:44 +0500

    Update Lab_02_task2.c

--- Lab_02_task2.c
+++ Lab_02_task2.c
@@ -1,8 +1,18 @@
 /*Task 2: Write a program, to calculate perimeter of an irregular connected shape having five sides. Hardcode the value of all sides in variables. Obtain perimeter by adding sides. Finally, print all the sides and perimeter*/
 
-#include<stdio.h>
-int main()
-{
-    //Perimeter of irregular shape having 5 sides
-    printf("Hello");
+#include <stdio.h>
+int main() {
+    int side_1=10;
+    int side_2=15;
+    int side_3=20;
+    int side_4=25;
+    int side_5=30;
+    int perimeter=side_1+side_2+side_3+side_4+side_5;
+    printf("side_1=%d",side_1);
+    printf("\nside_2=%d",side_2);
+    printf("\nside_3=%d",side_3);
+    printf("\nside_4=%d",side_4);
+    printf("\nside_5=%d",side_5);
+    printf("\nPerimeter = %d",perimeter);
+    return 0;
 }
