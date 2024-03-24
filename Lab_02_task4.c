d9eee52291d374ab4344f04c01794667a5c42766
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-24 23:18:12 +0500

    task_04

---
+++ Lab_02_task4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int wheat, rice, biscuit, total;
+    wheat=150;
+    rice=285;
+    biscuit=320;
+    total=wheat+rice+biscuit;
+    printf("Wheat\t   %d",wheat);
+    printf("\nRice\t   %d",wheat);
+    printf("\nBiscuit\t   %d",wheat);
+    printf("\n-----------------");
+    printf("\nTotal\t   %d",total);
+    return 0;
+}
