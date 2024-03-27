e8128d42f200afaebb5f57fd1444ed665cea22f2
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-03-27 13:18:07 +0500

    Update Lab_03_task2.c

--- Lab_03_task2.c
+++ Lab_03_task2.c
@@ -1,4 +1,27 @@
 /*
+Task 2: Write a program to input the amount (deposited in the bank). Calculate and print the amount after each year for three years, where compound interest rate is 10%:
+*/
+
+#include<stdio.h>
+int main(){
+
+float amount_deposited;
+float interest_rate =0.10;
+int years= 3;
+
+printf("Enter deposited amount:");
+scanf("%f", &amount_deposited);
+
+for(int i=1; i<=years; ++i){
+(amount_deposited += amount_deposited*0.10);
+printf("amount after year %d: %.2f\n",i,amount_deposited);
+}
+return 0;
+}
+
+//using Loop
+/*
+/*
 Task 2: Write a program to input the amount (deposited in the bank). Calculate and print the amount 
 after each year for three years, where compound interest rate is 10%:
 */
@@ -6,16 +29,19 @@ after each year for three years, where compound interest rate is 10%:
 #include<stdio.h>
 int main()
 {
-    int amount;
+    int deposit;
+    int years;
+    int interest;
     printf("Enter Deposited Amount: ");
-    scanf("%d",&amount);
-    int interest=amount + (amount * 0.1);
-    printf("Amount after 1 year= %d", interest);
-    amount=interest;
-    interest=amount + (amount * 0.1);
-    printf("\nAmount after 2 year= %d", interest);
-    amount=interest;
-    interest=amount + (amount * 0.1);
-    printf("\nAmount after 3 year= %d", interest);
+    scanf("%d",&deposit);
+    printf("Enter Number of Years: ");
+    scanf("%d",&years);
+    for(int i=1;i<=years;i++)
+    {
+        interest=deposit*0.1;
+        deposit=interest+deposit; 
+        printf("\nAmount after %d years = %d",i,deposit);
+    }
     return 0;
 }
+*/
