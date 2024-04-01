b20f2f343358f75bc05791fcce2b879baa32e6b7
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-04-01 21:39:38 +0500

    Update Lab_04_task2.c

--- Lab_04_task2.c
+++ Lab_04_task2.c
@@ -1,54 +1,3 @@
-
-/*
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-int main() {
-    time_t t1;
-    srand((unsigned)time(&t1));
-    
-    int numbers[4];
-    
-    // Generate four random numbers in the range 1-5 and print them
-    printf("Generated numbers: ");
-    for(int i = 0; i < 4; i++) {
-        numbers[i] = rand() % 5 + 1;
-        printf("%d ", numbers[i]);
-    }
-    
-    // Check if all numbers are different or if there are duplicates
-    int unique_count = 0;
-    for(int i = 0; i < 4; i++) {
-        int is_unique = 1;
-        for(int j = i + 1; j < 4; j++) {
-            if(numbers[i] == numbers[j]) {
-                is_unique = 0;
-                break;
-            }
-        }
-        if(is_unique) {
-            unique_count++;
-        }
-    }
-    
-    // Print the appropriate message based on the analysis
-    if(unique_count == 4) {
-        printf("\nAll are different\n");
-    } else if(unique_count == 2) {
-        printf("\nTwo are same and two are different\n");
-    } else {
-        printf("\nMore than two numbers are same\n");
-    }
-    
-    return 0;
-}
-
-*/
-
-
-#3
-/*
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
@@ -100,4 +49,3 @@ num1==num4&&num3==num2&&num1!=num3)
     }
 }
 
-*/
