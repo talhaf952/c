f4884ab13fa8f7b2bea3ed6714ce521f309f5737
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-04-04 20:14:56 +0500

    task_1

---
+++ Lab_07_task1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    float root1,root2;
+    printf("Input \"a\":");
+    scanf("%d",&a);
+    printf("Input \"b\":");
+    scanf("%d",&b);
+    printf("Input \"c\":");
+    scanf("%d",&c);
+    while(a>0)
+    {
+        
+    }
+    return 0;
+}
