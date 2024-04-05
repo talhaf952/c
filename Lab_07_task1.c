49eaf95cd3f70243abdd9e2f55afe98398f73bfa
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-04-05 23:10:10 +0500

    Update Lab_07_task1.c

--- Lab_07_task1.c
+++ Lab_07_task1.c
@@ -1,17 +1,31 @@
 #include<stdio.h>
-int main()
-{
-    int a,b,c;
-    float root1,root2;
-    printf("Input \"a\":");
-    scanf("%d",&a);
-    printf("Input \"b\":");
-    scanf("%d",&b);
-    printf("Input \"c\":");
-    scanf("%d",&c);
-    while(a>0)
-    {
-        
+#include<math.h>
+
+int main() {
+    int a, b, c;
+    float discriminant, root1, root2;
+    
+    do {
+        printf("Enter Non Zero value of \"a\": ");
+        scanf("%d", &a);
+    } while (a == 0);
+    
+    printf("Enter value of \"b\": ");
+    scanf("%d", &b);
+    
+    printf("Enter value of \"c\": ");
+    scanf("%d", &c);
+    
+    discriminant = (b * b) - (4 * a * c);
+    
+    if (discriminant < 0) {
+        printf("Sorry, the roots are imaginary.\n");
+    } else {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        printf("Root 1: %.2f\n", root1);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Root 2: %.2f", root2);
     }
+    
     return 0;
 }
