35757c818dfebc9cd468a71ed8a83d80ce40e1fd
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-04-05 23:10:52 +0500

    Update Lab_07_task1.c

--- Lab_07_task1.c
+++ Lab_07_task1.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<math.h>
-
 int main() {
     int a, b, c;
     float discriminant, root1, root2;
@@ -8,10 +7,8 @@ int main() {
         printf("Enter Non Zero value of \"a\": ");
         scanf("%d", &a);
     } while (a == 0);
-    
     printf("Enter value of \"b\": ");
     scanf("%d", &b);
-    
     printf("Enter value of \"c\": ");
     scanf("%d", &c);
     discriminant = (b * b) - (4 * a * c);
@@ -23,6 +20,5 @@ int main() {
         root2 = (-b - sqrt(discriminant)) / (2 * a);
         printf("Root 2: %.2f", root2);
     }
-    
     return 0;
 }
